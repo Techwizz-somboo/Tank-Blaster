07daa03e6334f5b7a9fae20ad7a4584edae66b69
Techwizz-somboo <paulrblack.prb@gmail.com>
2022-03-02 23:03:24 -0700

    Tank files

---
+++ Objects/Player-tank/Player.gd
@@ -0,0 +1,38 @@
+extends KinematicBody
+
+onready var camera = get_node("Camera")
+var rayOrigin = Vector3()
+var rayEnd = Vector3()
+var speed = 15
+var gravity = 100
+var velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	
+	var space_state = get_world().direct_space_state
+	var mouse_position = get_viewport().get_mouse_position()
+	rayOrigin = camera.project_ray_origin(mouse_position)
+	rayEnd = rayOrigin + camera.project_ray_normal(mouse_position) * 2000 # Set cursor position to where the raycast hit
+	var intersection = space_state.intersect_ray(rayOrigin, rayEnd) # Set variable for cursor position
+	if not intersection.empty(): # Make sure there is a intersection with raycast
+		var pos = intersection.position
+		$Tank.look_at(Vector3(pos.x, pos.y, pos.z), Vector3(0,1,0))
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+#		$Pivot.look_at(translation + direction, Vector3.UP)
+	
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	velocity.y -= gravity * delta
+	velocity = move_and_slide(velocity, Vector3.UP)
