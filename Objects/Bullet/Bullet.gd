bc16fd0513023b35a9ebf81f57d34b046ffce2cb
Techwizz <paulrblack.prb@gmail.com>
2022-03-05 22:44:30 -0700

    Bullets now check for when they collide with a tank

--- Objects/Bullet/Bullet.gd
+++ Objects/Bullet/Bullet.gd
@@ -12,6 +12,16 @@ func _physics_process(delta):
 #	velocity += g * delta  # Uncomment this for bullet gravity
 	look_at(transform.origin + velocity.normalized(), Vector3.UP)
 	transform.origin += velocity * delta
+	
+	for body in get_overlapping_bodies():  ### HERE IS WHERE THE BULLET CHECKS FOR TANK COLLISIONS
+		if get_overlapping_bodies().has(get_parent().get_node("PlayerTank")):
+			print ("Found Player Tank")
+			self.queue_free()
+			body.queue_free()
+		if get_overlapping_bodies().has(get_parent().get_node("AITank00")):
+			print ("Found AI Tank")
+			self.queue_free()
+			body.queue_free()
 
 
 func _on_Shell_body_entered(body):
